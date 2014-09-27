f64a7be017aca0a6185ecefc18a1b692ef517bd6
KharitonovDEV <alexander.kharitonov.odessa@gmail.com>
2014-09-28 01:08:07 +0300

    NewFile

---
+++ application/views/add_user.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?echo $title?></title>
+</head>
+<body>
+
+<form method="post" action="add_user_action">
+NICK: <input type="text"><br>
+Pass: <input type="text"><br>
+</form>
+
+</body>
+</html>
